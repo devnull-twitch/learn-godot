df9e11cfde459fa8e16e566bf91041776283311b
devnull-twitch <97809736+devnull-twitch@users.noreply.github.com>
2022-07-03 20:45:01 +0200

    add some documentation and fixed minor bugs

--- src/Navigation2D/Charcter.gd
+++ src/Navigation2D/Charcter.gd
@@ -1,31 +1,87 @@
+# This script is assigned to the main player node.
+# The main player is controlled with RTS like controls. A player click on the
+# map will make the player character go to the clicked position. 
+
+# The base node for the main character is a KinematicBody2D this allows us
+# to do physics based moivements later on.
 extends KinematicBody2D
 
+# We set a custom class name for this node so we reference this specific node
+# type in FollowMeAgent.gd as the target for the follower.
 class_name Character
 
+# target is last clicked position of the player. This is set within the
+# _unhandled_input method.
 var target = null
+
+# Getting a reference of two other nodes in this character scene.
 onready var nav_agent = $NavigationAgent2D;
 onready var nav_path = get_node("../PlayerPath")
 	
+# _unhandled_input is called whener any button press or mouse movement, clicks,
+# or any other user input occurs.
 func _unhandled_input(event):
+	
+	# We only care for mouse button events
 	if event is InputEventMouseButton:
+		
+		# We type cast the event to be a mouse button event so we can read 
+		# properties exclusive to a InputEventMouseButton
 		var mouse_event = event as InputEventMouseButton
-		if mouse_event.button_index == BUTTON_LEFT:
+		
+		# The explusive iknfo we need is the button index that is pressed.
+		# If we do not check this a right mouse button click would also 
+		# make the character move.	
+		if mouse_event.is_pressed() and mouse_event.button_index == BUTTON_LEFT:
+			
+			# You can get the current mouse pointer location in world space
+			# at any time from a node using get_global_mouse_position()
 			var mouse_target = get_global_mouse_position()
+			
+			# We use that position as the taret for the character
 			nav_agent.set_target_location(mouse_target)
 			target = nav_agent.get_final_location()
 
+
+# _physics_process is called once every physics frame.
 func _physics_process(delta):
+	
+	# If we are currently having a target position to go to
 	if target != null:
-		nav_agent.set_target_location(target)
+		# we get the next point on our path to the target
 		var next_location = nav_agent.get_next_location()
-		var unsafe_velocity =  global_position.direction_to(next_location).normalized() * 100		
+		# and calculate a vector that represents the velocity we want the
+		# character to have so it will reach the next location on the path.
+		var unsafe_velocity =  global_position.direction_to(next_location).normalized() * 100
+		# But this would not take into account obstacles or other agants.
+		# We dont want to collide with anyone, so we ask the godot navigation
+		# service to calculate a safe velocity that takles our surroundings
+		# into account.	
 		nav_agent.set_velocity(unsafe_velocity)
 
+# _on_NavigationAgent2D_velocity_computed is a method called by a signal from
+# the NavigationAgent2D with  a safe velocity. It is guaranteed that we will 
+# not run into anything using the velocity.  
 func _on_NavigationAgent2D_velocity_computed(safe_velocity):
+	# If we are slow we might be running into an obstacle so lets see if we can
+	# identfy a better path by re-calculating the path to the target.
+	# You could also do this in _physics_process if you dont have many agents
+	# in your game and as such performance is not an issue for you. 
+	if safe_velocity.length() <= 35:
+		nav_agent.set_target_location(target)
+	
+	# move_and_slide is a method provided by KinematicBody2D and will do the
+	# actual movement
 	move_and_slide(safe_velocity)
 
-func _on_NavigationAgent2D_target_reached():
-	target = null;
-
+# This method is conencted to a signal from the agent, and is called when the 
+# path to the target is updated. We use this to update the rendering of the
+# path preview you see for the character. 
 func _on_NavigationAgent2D_path_changed():
 	nav_path.points = nav_agent.get_nav_path()
+
+# _NavigationAgent2D_navigation_finished is also a method called be an agent
+# signal and removes the current target because we have reached the closest 
+# point we can to the clicked position.
+func _on_NavigationAgent2D_navigation_finished():
+	target = null;
--- src/Navigation2D/FollowMeAgent.gd
+++ src/Navigation2D/FollowMeAgent.gd
@@ -1,11 +1,29 @@
+# The FollowMeAgent is placed on a kinematic body that will alws try to reach
+#
+# a position close to the character supplyed in the editors property
+# "Path to player"
+
 extends KinematicBody2D
 
+# This creates a field in the editor UI for you to assign the target character
+# node to follow. This will contain a string as ap node path to the character
+# node and not a direct reference of said node.
 export(NodePath) var path_to_player
+
+# player_node will contain a reference to the actual node after the 
+# ready function is complete
 var player_node: Character
+
+# A few references to other nodes in this scene
 onready var nav_agent = $NavigationAgent2D
 onready var sprite = $Sprite
+
+# This internal state represents if the node is curretly following the
+# character or not. You can change that by clicking on the follower
+# in the game.
 var active_follow = false setget update_active_follow
 
+# Just a few color values for modulation to visualize the active state
 const active_color = Color(1, 1, 0, 1)
 const inactive_color = Color(1, 1, 1, 1)
 
@@ -13,37 +31,65 @@ const inactive_color = Color(1, 1, 1, 1)
 func _ready():
 	player_node = get_node(path_to_player)
 
+# _physics_process is called once every physics frame
 func _physics_process(delta):
+	# If we do not have a reference to a node to follow or we are not in an
+	# active follow state we dont do anything.
 	if player_node == null or !active_follow:
 		return
 	
+	# Otherwise we take the current target position as our target position to move to.
 	nav_agent.set_target_location(player_node.global_position)
-	if nav_agent.is_target_reached():
-		return
-		
-	var d = global_position.distance_to(player_node.global_position)
-	if d < 90:
+	
+	# If we are close enough to the character we dont do anything.
+	if nav_agent.is_navigation_finished():
 		return
 	
+	# If we are not we get the next point oin our path to the player.
 	var next_location = nav_agent.get_next_location()
-	var unsafe_velocity =  global_position.direction_to(next_location).normalized() * 50			
+	# We then calculate a unsafe velocity. So If we use this without collision avoidance we might
+	# run into a physics object or other agent. 
+	var unsafe_velocity =  global_position.direction_to(next_location).normalized() * 50
+	# So we ask the navigation system to calculate a safe velocity			
 	nav_agent.set_velocity(unsafe_velocity)
 
-
+# _on_NavigationAgent2D_velocity_computed is called when a safe velocity is calculated
 func _on_NavigationAgent2D_velocity_computed(safe_velocity):
+	# We use that velocity to let the physics engione move the follower.
 	move_and_slide(safe_velocity)
 
-
-func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		var mouse_event = event as InputEventMouseButton
-		if mouse_event.is_pressed() and mouse_event.button_index == BUTTON_LEFT:
-			update_active_follow(!active_follow)
-			get_tree().set_input_as_handled()
-			
+# set state and update modulate color of the sprite to visualize follow state
 func update_active_follow(new_val):
 	active_follow = new_val
 	if new_val:
 		sprite.modulate = active_color
 	else:
 		sprite.modulate = inactive_color
+
+# If the follower is clicked we do not want to move to that.
+# Because of the order of methods we need a few methods to prevent this.
+# If you read the documentation you see an area has a "input_event" signal.
+# But have a look at this page:
+# https://docs.godotengine.org/en/stable/tutorials/inputs/inputevent.html#how-does-it-work
+# The collision methods are called even after the "_unhandled_input" in the character.
+# So the better option is to detect when the mouse enter or leaves and then handle clicks in 
+# _input. 
+
+var mouse_in_are: bool = false
+
+func _input(event):
+	# same as in Character we check if the input event is a mouse button click
+	if mouse_in_are and event is InputEventMouseButton:
+		# we then check if the left mouse button was clicked
+		var mouse_event = event as InputEventMouseButton
+		if mouse_event.is_pressed() and mouse_event.button_index == BUTTON_LEFT:
+			# and update the internal active follow state
+			update_active_follow(!active_follow)
+			# and we also mark this click as handled. So we dont trigger a movement to this location.
+			get_tree().set_input_as_handled()
+
+func _on_Area2D_mouse_entered():
+	mouse_in_are = true
+
+func _on_Area2D_mouse_exited():
+	mouse_in_are = false
